2ed0f2fb28db7a8107fae493d345e17e0d058957
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-07-15 10:00:48 -0700

    Buggy Ex1.8
    Code runs in an infinite loop; needs to be fixed.

--- Chapter 1/1.8.scm
+++ Chapter 1/1.8.scm
@@ -1,11 +1,26 @@
 ;; Newton's method for cube roots
 
-(define (cubeRoot x) (
-    (cube-root-iter 1.0 x)    		      
-))
+(define (cube-root x) (
+    (cube-root-iter 1.0 x)))
 
 (define (cube-root-iter guess x)
-  (if good-enough? guess x)
+  (if good-enough? guess (improve guess x))
   guess
   (cube-root-iter (improve guess x) x)
 )
+
+(define (good-enough? guess next-guess)
+  (< (abs (- guess next-guess))
+     (* 0.001 guess)))
+
+(define cube (lambda (x) (* x x x)))
+
+(define (square x) (* x x))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+	(* guess 2))
+     3))
+
+;; use methods
+(cube-root 1.0)
