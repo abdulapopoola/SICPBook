934a8c7f6b00d22e9332588a62f7f876d8c1c550
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-07-16 09:59:16 -0700

    Ex1.8 bug fix
    1. Syntax error (not evaluating good-enough because it was not between
    braces) got the code stuck in an infinite loop
    2. Extra set of braces in the cube-root method meant that the result was
    seen as a function

--- Chapter 1/1.8.scm
+++ Chapter 1/1.8.scm
@@ -1,26 +1,26 @@
 ;; Newton's method for cube roots
 
-(define (cube-root x) (
-    (cube-root-iter 1.0 x)))
-
-(define (cube-root-iter guess x)
-  (if good-enough? guess (improve guess x))
-  guess
-  (cube-root-iter (improve guess x) x)
-)
+(define (square x) (* x x))
 
 (define (good-enough? guess next-guess)
-  (< (abs (- guess next-guess))
-     (* 0.001 guess)))
-
-(define cube (lambda (x) (* x x x)))
-
-(define (square x) (* x x))
+  (< (abs (- next-guess guess))
+     (* 1.0e-20 guess)))
 
 (define (improve guess x)
   (/ (+ (/ x (square guess))
-	(* guess 2))
+     (* 2 guess))
      3))
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess (improve guess x))
+  guess
+  (cube-root-iter (improve guess x) x)))
+
+(define (cube x) (* x x x))
+
+(define (cube-root x) (cube-root-iter 1.0 x))
 
 ;; use methods
 (cube-root 1.0)
+(cube-root 27.0)
+(cube-root 64.0)
