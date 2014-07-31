09474b76eccf80f32373434cabd57f595f0d949c
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-07-31 10:07:51 -0700

    Ex1.10 second half
    Complete ex1.10

--- Chapter 1/1.10.scm
+++ Chapter 1/1.10.scm
@@ -54,4 +54,16 @@
 ;; (A 2 (A 2 2))
 ;; (A 2 4); --> (A 2 2) = 4 from above
 ;; 65536; --> From above
-;; 
+;;
+
+
+(define (f n) (A 0 n))
+;; 2*n
+
+(define (g n) (A 1 n))
+;; 2^n
+
+(define (h n) (A 2 n))
+;; 2^2^2...2 (n times)
+
+
