916dc43a2f443c5fe08580898afc817fbff3d8cc
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-08-03 17:07:07 -0700

    Ex 1.11
    First half of Ex1.11

---
+++ Chapter 1/1.11.scm
@@ -0,0 +1,16 @@
+;; Recursive process
+
+(define (f n) 
+  (if (< n 3) 
+    n
+    (+ (f (- n 1)) 
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3))))))
+
+;; Iterative process
+(define (f n) 
+  (if (< n 3) 
+    n
+    (+ (f (- n 1)) 
+       (* 2 (f (- n 2)))
+       (* 3 (f (- n 3))))))
