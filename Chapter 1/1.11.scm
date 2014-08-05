91d1ec919748bf5072a7dee4b00f01a5d92c6e66
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-08-05 10:01:57 -0700

    Ex1.11
    Iterative process for question; needs to be analyzed and understood
    again.

--- Chapter 1/1.11.scm
+++ Chapter 1/1.11.scm
@@ -8,9 +8,16 @@
        (* 3 (f (- n 3))))))
 
 ;; Iterative process
+(define (f-iter a b c n)
+  (if (= n 0)
+    c
+    (f-iter (+ a (* 2 b) (* 3 c))
+	    a
+	    b
+	    (- n 1))))
+
 (define (f n) 
   (if (< n 3) 
     n
-    (+ (f (- n 1)) 
-       (* 2 (f (- n 2)))
-       (* 3 (f (- n 3))))))
+    (f-iter 2 1 0 n)
+
