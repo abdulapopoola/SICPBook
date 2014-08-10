06afcc87f534a5c7b52f8faaa47e55246478cd8d
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-08-10 11:09:01 -0700

    Improved solution to Ex 1.11

--- Chapter 1/1.11.scm
+++ Chapter 1/1.11.scm
@@ -21,3 +21,16 @@
     n
     (f-iter 2 1 0 n)
 
+;; Better iterative process; doesn't do redundant calculations
+(define (f-iter a b c n)
+  (if (= n 0)
+    a
+    (f-iter (+ a (* 2 b) (* 3 c))
+	    a
+	    b
+	    (- n 1))))
+
+(define (f n) 
+  (if (< n 3) 
+    n
+    (f-iter 2 1 0 (- n 2))
