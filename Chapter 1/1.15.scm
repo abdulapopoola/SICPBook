c4482c0006ebd6c5bd6b81cb49e296519a6e6005
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-08-20 09:44:54 -0700

    Ex 1.5
    Ex 1.5's  solution

---
+++ Chapter 1/1.15.scm
@@ -0,0 +1,19 @@
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+   (if (not (> (abs angle) 0.1))
+       angle
+       (p (sine (/ angle 3.0)))))
+
+;; (sine 12.15)
+;; (p (sine 4.05))
+;; (p (p (sine 1.35)))
+;; (p (p (p (sine 0.45))))
+;; (p (p (p (p (sine 0.15)))))
+;; (p (p (p (p (p (sine 0.05))))))
+;;
+;; 5 times
+;;
+;;
+;; Steps: O(log n)
+;; Space: O(log n); only log n inputs need to t be stored as we get to calculating the value. 
