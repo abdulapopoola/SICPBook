52e440795b5cd70ad8b858a756ed5c20c9607e90
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-08-29 10:09:40 -0700

    Update to Ex 1.17
    Added support for zero
    Need to update support for negative numbers.

--- Chapter 1/1.17.scm
+++ Chapter 1/1.17.scm
@@ -3,8 +3,9 @@
 (define (halve x) (/ x 2))
 
 (define (fast-mult x n)
-  (cond ((= 1 n) x)
-	((even? n) (double x) (halve n))
+  (cond ((or (zero? x) (zero? n)) 0)
+    	((= 1 n) x)
+	((even? n) (fast-mult (double x) (halve n)))
 	((odd? n) (+ x (fast-mult x (- n 1))))))
 
 (fast-mult 2 10)
