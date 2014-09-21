6f05a850553775c5cc0e31528e00b6506912bddd
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-09-21 10:53:59 -0700

    Ex 1.22

---
+++ Chapter 1/1.22.scm
@@ -0,0 +1,26 @@
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) 
+                       start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? x y)
+  (= (remainder y x) 0))
+
+(define (square n) (* n n))
+
+(define (prime? n) (= (smallest-divisor n) n))
