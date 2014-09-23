08dc485c69ff518129767094df3bd9eeed102177
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-09-23 10:07:28 -0700

    Working on 1.22
    List of steps to follow for solution

--- Chapter 1/1.22.scm
+++ Chapter 1/1.22.scm
@@ -1,11 +1,15 @@
+#lang planet neil/sicp
+
 (define (timed-prime-test n)
   (newline)
   (display n)
   (start-prime-test n (runtime)))
+
 (define (start-prime-test n start-time)
   (if (prime? n)
       (report-prime (- (runtime) 
                        start-time))))
+
 (define (report-prime elapsed-time)
   (display " *** ")
   (display elapsed-time))
@@ -24,3 +28,11 @@
 (define (square n) (* n n))
 
 (define (prime? n) (= (smallest-divisor n) n))
+
+(define (search-for-primes start end)
+  (#t))
+;;start number
+;;how many needed
+;;check if even, if even, +1 with same function
+;;else, call timed-prime-test with start number
+;;; continue calling with odd+2 until found gets to zero
