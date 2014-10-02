2df0c96457d2b8176a00ca00760a059b1cabb280
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-10-02 09:41:42 -0700

    Ex1.22 completed

--- Chapter 1/1.22.scm
+++ Chapter 1/1.22.scm
@@ -1,18 +1,18 @@
 #lang planet neil/sicp
 
 (define (timed-prime-test n)
-  (newline)
-  (display n)
   (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
   (if (prime? n)
-      (report-prime (- (runtime) start-time))
-      (#f)))
+      (report-prime n (- (runtime) start-time))
+      false))
 
-(define (report-prime elapsed-time)
+(define (report-prime n elapsed-time)
+  (display n)
   (display " *** ")
-  (display elapsed-time))
+  (display elapsed-time)
+  (newline))
 
 (define (smallest-divisor n)
   (find-divisor n 2))
@@ -30,10 +30,26 @@
 (define (prime? n) (= (smallest-divisor n) n))
 
 (define (search-for-primes startNum primesCount)
-  (cond ((= primesCount 0))
+  (cond ((= primesCount 0) (values))
         ((even? startNum) (search-for-primes (+ startNum 1) primesCount))
-        (if (timed-prime-test startNum) 
-            (search-for-primes (+ startNum 2) (- primesCount 1))
-            (search-for-primes (+ startNum 2) primesCount))))
+        (else (if (timed-prime-test startNum) 
+                  (search-for-primes (+ startNum 2) (- primesCount 1))
+                  (search-for-primes (+ startNum 2) primesCount)))))
 
-(search-for-primes 1000 3)
+(search-for-primes 1000 3) ;;1009, 1013, 1019
+(newline)
+(search-for-primes 10000 3) ;;10007, 10009, 10037
+(newline)
+(search-for-primes 100000 3) ;; 100003, 100019, 100043
+(newline)
+(search-for-primes 1000000 3) ;; 1000003, 1000033, 1000037
+(newline)
+
+;;These are still too fast so using larger numbers
+(search-for-primes 100000000 2); approximately 1000
+(newline)
+(search-for-primes 1000000000 2); approximately 3000
+(newline)
+(search-for-primes 10000000000 2); approximately 10000
+
+;;For a factor of 10 increase, runtime increases by approx (sqrt 10) ~ 3.16...
