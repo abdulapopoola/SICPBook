fa7b42a26e6f4aabe64aac50324b6cc42884e515
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-10-12 08:35:12 -0700

    Ex1.27 fixed and now correct

--- Chapter 1/Ex1.27.scm
+++ Chapter 1/Ex1.27.scm
@@ -1,18 +1,16 @@
 #lang planet neil/sicp
 
 (define (congruent-modulo n)  
-  (define (try-it a)
-    (if (= a 0)
-        (display "Carmicheal number")
-        (= (expmod a n n) a)))
-  (if (try-it (- n 1))
-      (try-it (- n 1))
-      (display "Not a Carmicheal number")))
-
-
-;; start from 1 - n -1
-;; check if it is congruent modulo,
-;; else return false
+  (display n)
+  (if (check 1 n)      
+      (display " is a Carmicheal number\n")
+      (display " is not a Carmicheal number\n")))
+  
+(define (check a n)    
+  (cond ((= a n) true)
+        ((= (expmod a n n) a)
+         (check (+ 1 a) n))
+        (else false)))
 
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
@@ -26,3 +24,11 @@
           m))))
 
 (define (square n) (* n n))
+
+(congruent-modulo 10)
+
+(congruent-modulo 561)
+(congruent-modulo 1105)
+(congruent-modulo 1729)
+(congruent-modulo 2465)
+(congruent-modulo 2821)
