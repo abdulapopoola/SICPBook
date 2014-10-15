33b5957b1996e08b0789a38b9ee2a9caeaf263e7
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-10-15 09:00:49 -0700

    Ex1.28 initial attempt
    Not complete yet; trying to understand the question

---
+++ Chapter 1/Ex1.28.scm
@@ -0,0 +1,14 @@
+#lang planet neil/sicp
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder 
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder 
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (square n) (* n n))
