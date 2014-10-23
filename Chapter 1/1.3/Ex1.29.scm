2c8a6cb8264a546fe7f59727ece6a25d49c10bfa
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-10-23 08:38:58 -0700

    Ex1.29 nearly done
    Got help from Bill the Lizard

--- Chapter 1/1.3/Ex1.29.scm
+++ Chapter 1/1.3/Ex1.29.scm
@@ -1,3 +1,28 @@
+#lang planet neil/sicp
 ;; Simpson's rule
 ;; h = (b - a) /n
 ;; y(k) = f(a + kh)
+;; NEED TO UNDERSTAND, still abstruse
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (simpsons-rule f a b n)
+  (define h (/ (- b a) n))
+  (define (inc x) (+ x 1))
+  (define (y-k k) (f (+ a (* k h))))
+  (define (term k)
+    (* (cond ((odd? k) 4)
+             ((or (= k 0) (= k n)) 1)
+             ((even? k) 2))
+       (y-k k)))
+  (/ (* h (sum term 0 inc n)) 3))
+
+(define (cube x) (* x x x))
+
+(simpsons-rule cube 0 1 100)
+
+(simpsons-rule cube 0 1 1000)
