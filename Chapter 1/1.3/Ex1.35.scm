0ebf1f0f4dd4e136d9ede53f27fab1c6ed139b13
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-10-30 08:32:05 -0700

    Ex1.35

---
+++ Chapter 1/1.3/Ex1.35.scm
@@ -0,0 +1,18 @@
+#lang planet neil/sicp
+
+;; x -> 1 + 1/x is equivalent to
+;; x^2 -> x + 1 which is the golden ratio
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? next guess)
+          next
+          (try next))))
+  (try first-guess))
+
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
