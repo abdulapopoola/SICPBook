3c7c4b176f4772652ab2e08914015a9f75bf0817
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-11-12 20:09:59 -0800

    Ex2.2 completed

--- Chapter 2/2.1/Ex2.2.scm
+++ Chapter 2/2.1/Ex2.2.scm
@@ -1,5 +1,7 @@
 #lang planet neil/sicp
 
+(define (average x y) (/ (+ x y) 2))
+
 (define (make-point x y)
   (cons x y))
 
@@ -19,10 +21,11 @@
   (cdr segment))
 
 (define (midpoint-segment segment)
-  (let ((start
-;; get start and end points
-;; add their x and ys
-;; construct new point
+  (let ((start-point (start-segment segment))
+        (end-point (end-segment segment)))
+    (make-point 
+     (average (x-point start-point) (x-point end-point))
+     (average (y-point start-point) (y-point end-point)))))
 
 (define (print-point p)
   (newline)
@@ -31,3 +34,7 @@
   (display ",")
   (display (y-point p))
   (display ")"))
+
+(print-point 
+ (midpoint-segment 
+  (make-segment (make-point 2 2) (make-point 4 4))))
