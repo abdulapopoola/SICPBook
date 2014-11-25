e2dc186e42f795d4b1fd2cefcd503d5daf68dd55
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-11-24 20:35:31 -0800

    Ex2.06.scm

--- Chapter 2/2.1/Ex2.06.scm
+++ Chapter 2/2.1/Ex2.06.scm
@@ -4,3 +4,29 @@
 
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+;; one -> (add-1 zero)
+;; (lambda (f) (lambda (x) (f ((zero f) x)))))
+
+;; (zero f) -> ((lambda (f) (lambda (x) x)) f)
+;;             (lambda (x) x)
+
+;; Substituting into (add-1 zero)
+;; (lambda (f) (lambda (x) (f (lamda (x) x) x)))))
+;;    ->   (lambda (f) (lambda (x) (f x))))
+
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+;; two -> (add-1 one)
+;; (lambda (f) (lambda (x) (f ((one f) x)))))
+
+;; (one f) -> ((lambda (f) (lambda (x) (f x)))) f)
+;;            (lambda (x) (f x))
+
+;; Substituting into (add-1 one)
+;; (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x)))))
+;;    ->     (lambda (f) (lambda (x) (f (f x))))
+
+(define (church-addition m n)
+   (lambda (f) (lambda (x) ((m f) ((n f) x)))))
---
+++ Chapter 2/2.1/Ex2.06.scm~
@@ -0,0 +1,6 @@
+#lang planet neil/sicp
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
