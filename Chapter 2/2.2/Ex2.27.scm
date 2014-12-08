ea118188d39676573bb7d410c651cc633d0eff76
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-08 08:39:01 -0800

    Ex2.27.scm
    Fix for wrong execution

--- Chapter 2/2.2/Ex2.27.scm
+++ Chapter 2/2.2/Ex2.27.scm
@@ -3,13 +3,11 @@
 (define (deep-reverse list1)
   (define (iter items acc)
     (cond ((null? items) acc)
-          ((list? items) 
+          ((pair? (car items))
            (iter (cdr items) (cons (deep-reverse (car items)) acc)))
           (else
-           (iter '() (cons items acc)))))
+           (iter (cdr items) (cons (car items) acc)))))
   (iter list1 '()))
-
-;;(reverse (list 1 4 9 16 25))
 
 (define x 
   (list (list 1 2) (list 3 4)))
