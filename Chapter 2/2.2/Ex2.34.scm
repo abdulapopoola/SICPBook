a26f7920bf5659b7cf971b7174540d9c54a31980
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-14 10:56:05 -0800

    Ex2.34.scm

---
+++ Chapter 2/2.2/Ex2.34.scm
@@ -0,0 +1,21 @@
+#lang planet neil/sicp
+
+;; helpers
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op 
+                      initial 
+                      (cdr sequence)))))
+
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate 
+   (lambda (this-coeff higher-terms)
+     (+ this-coeff (* x higher-terms)))
+   0
+   coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
+;;79
