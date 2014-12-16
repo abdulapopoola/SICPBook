1951f1733b5a970e111b8aec240140eb8f6cf2ed
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-16 08:43:47 -0800

    Ex2.38.scm

---
+++ Chapter 2/2.2/Ex2.38.scm
@@ -0,0 +1,29 @@
+#lang planet neil/sicp
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+;;Recursive fold-left
+(define (fold-left-rec op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (fold-left-rec op initial (cdr sequence))
+          (car sequence))))
+
+(fold-right / 1 (list 1 2 3)); 3/2
+(fold-left  / 1 (list 1 2 3)); 1/6
+(fold-right list nil (list 1 2 3)); '(1 '(2 '(3 '())))
+(fold-left  list nil (list 1 2 3))
+
+;; Commutativity e.g A + B === B + A
