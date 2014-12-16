76aaf51a4b28ce19f8a3cb78e169643f71a868a2
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-16 09:04:36 -0800

    Ex2.39.scm updated

--- Chapter 2/2.2/Ex2.39.scm
+++ Chapter 2/2.2/Ex2.39.scm
@@ -8,18 +8,20 @@
           (fold-right op initial (cdr sequence)))))
 
 (define (fold-left op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (fold-left op initial (cdr sequence))
-          (car sequence))))
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
 
 (define (reverse sequence)
   (fold-right 
-   (lambda (x y) (cons y x)) nil sequence))
+   (lambda (x y) (append y (list x))) nil sequence))
 
 (define (reverse2 sequence)
   (fold-left 
-   (lambda (x y) (cons x y)) nil sequence))
+   (lambda (x y) (cons y x)) nil sequence))
 
 (reverse (list 1 4 9 16 25))
 (reverse2 (list 1 4 9 16 25))
