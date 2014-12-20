10e6f99e50b1974d12241401471ef3cb929cd610
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-20 10:46:18 -0800

    Ex2.42 nearly done

---
+++ Chapter 2/2.2/Ex2.42.scm
@@ -0,0 +1,49 @@
+#lang planet neil/sicp
+
+;;helpers
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate 
+                       (cdr sequence))))
+        (else  (filter predicate 
+                       (cdr sequence)))))
+
+(define empty-board '())
+
+(define (adjoin-position row col board)
+  (cons (cons row col) board))
+
+(define (get-row position)
+  (car position))
+
+(define (get-column position)
+  (cdr position))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) 
+           (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position 
+                    new-row 
+                    k 
+                    rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
