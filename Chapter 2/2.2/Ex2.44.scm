8844e0252a5265a9051f77d94cf8e171b287643a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-22 08:34:55 -0800

    Ex2.44.scm

---
+++ Chapter 2/2.2/Ex2.44.scm
@@ -0,0 +1,9 @@
+#lang planet neil/sicp
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter 
+                                  (- n 1))))
+        (below painter 
+                (beside smaller smaller)))))
