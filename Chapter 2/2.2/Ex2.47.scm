a7d5a8d440cb45a024741a2f0f7ea090fc96e2db
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-23 08:31:19 -0800

    Ex2.46.scm

---
+++ Chapter 2/2.2/Ex2.47.scm
@@ -0,0 +1,25 @@
+#lang planet neil/sicp
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define (origin-frame f)
+  (car f))
+
+(define (edge1-frame f)
+  (cadr f))
+
+(define (edge2-frame f)
+  (caddr f))
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define (origin-frame f)
+  (car f))
+
+(define (edge1-frame f)
+  (cadr f))
+
+(define (edge2-frame f)
+  (cddr f))
