2fd5cce010bb2a5af93e4e1fd4ea3cecbbaef8f2
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-24 20:53:58 -0800

    Ex2.48.scm

---
+++ Chapter 2/2.2/Ex2.48.scm
@@ -0,0 +1,11 @@
+#lang planet neil/sicp
+
+;; helpers
+(define (make-segment v1 v2)
+  (cons v1 v2))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
