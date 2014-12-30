2f191d0e632562b2f12795a620b302a604cb0d9b
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-12-30 08:33:50 -0800

    Ex2.57 and Ex2.58a

---
+++ Chapter 2/2.3/Ex2.57.scm
@@ -0,0 +1,11 @@
+#lang planet neil/sicp
+
+(define (augend s)
+  (if (null? (cdddr s))
+      (caddr s)
+      (cons '+ (cddr s))))
+
+(define (multiplicand p)
+  (if (null? (cdddr p))
+      (caddr p)
+      (cons '* (cddr p))))
---
+++ Chapter 2/2.3/Ex2.58.scm
@@ -0,0 +1,73 @@
+#lang planet neil/sicp
+
+;; helpers
+(define (variable? x) (symbol? x))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1)
+       (variable? v2)
+       (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) 
+         (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) 
+             (=number? m2 0)) 
+         0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) 
+         (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+(define (addend s) (car s))
+(define (augend s) (caddr s))
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+(define (multiplier p) (car p))
+(define (multiplicand p) (caddr p))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+(define (base p) (cadr p))
+(define (exponent p) (caddr p))
+(define (make-exponentiation base exp)
+  (cond ((=number? exp 0) 1)
+        ((=number? exp 1) base)
+        ((and (number? base) (number? exp)) (expt base exp))
+        (else (list '**  base exp))))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product 
+           (multiplier exp)
+           (deriv (multiplicand exp) var))
+          (make-product 
+           (deriv (multiplier exp) var)
+           (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (exponent exp)
+                       (make-product (make-exponentiation (base exp)
+                                                          (make-sum (exponent exp) -1))
+                                     (deriv (base exp) var))))
+        (else (error "unknown expression 
+                      type: DERIV" exp))))
+
+(deriv '(x + (3 * (x + (y + 2)))) 'x)
