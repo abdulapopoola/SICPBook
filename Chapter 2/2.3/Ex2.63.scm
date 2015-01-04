b3e471d3eb6d91df3b0dddac8f89795a0347fef6
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-01-04 13:40:24 -0800

    Ex2.63.scm

---
+++ Chapter 2/2.3/Ex2.63.scm
@@ -0,0 +1,26 @@
+#lang planet neil/sicp
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append 
+       (tree->list-1 
+        (left-branch tree))
+       (cons (entry tree)
+             (tree->list-1 
+              (right-branch tree))))))
+;; Both produce the same results
+;; This will take the lists and pass them straight to the append operation (which is O(N)); so this is O(N**N) growth.
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list 
+         (left-branch tree)
+         (cons (entry tree)
+               (copy-to-list 
+                (right-branch tree)
+                result-list)))))
+  (copy-to-list tree '()))
+;; This is recursive and will walk through each list every time. This is O(N) growth
