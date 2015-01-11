7f8e2a735d3a07988cb5fd1882d053009f2b3f35
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-01-11 11:11:38 -0800

    Ex2.69.scm

--- Chapter 2/2.3/Ex2.69.scm
+++ Chapter 2/2.3/Ex2.69.scm
@@ -1,3 +1,12 @@
 #lang planet neil/sicp
 
+;;Requires code from earlier exercises
+
 (define (successive-merge pairs)
+  (if (= (length pairs) 1)
+      pairs
+      (successive-merge
+       (adjoin-set (make-code-tree (car pairs)
+                                   (cadr pairs)) 
+                   (cddr pairs)))))
+
