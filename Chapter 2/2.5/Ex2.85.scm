f2e0588b8d89ae046d89aa152298a9bd58fe3d4a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-02-11 08:18:26 -0800

    Ex2.85

--- Chapter 2/2.5/Ex2.85.scm
+++ Chapter 2/2.5/Ex2.85.scm
@@ -1,8 +1,29 @@
 #lang planet neil/sicp
+;; INSTALLATION
+;; Integer number package
+;; Internal implementation
+(define (rational->integer rat)
+  (make-integer (round (/ (numer rat) (denom rat)))))
 
-(define (type-tag val) (car val))
-(define (contents val) (cdr val))
-(define (get-lower type other) #t)
+;; External interface
+(put-coercion 'rational 'integer rational->integer)
+
+;; Rational number package
+;; Internal procedure
+(define (real->rational real)
+  (make-rational (inexact-exact (numerator real))
+                 (inexact-exact (denominator real))))
+                                
+;; External interface
+(put-coercion 'real 'rational real->rational)
+
+;; Real number package
+;; Internal procedure
+(define (complex->real complex)
+  (make-real (complex-real-part complex)))
+
+;; External interface
+(put-coercion 'complex 'real real->complex)
 
 (define type-tower '(integer rational real complex))
 
@@ -18,4 +39,30 @@
                      (lower-operator (contents val))
                      (error "No lower operator exists for these types"
                             (list (type-tag val) (cdr val-type-tower)))))))))
+
+;; Drop can also be achieved by passing in a reversed tower to the raise operator
+;; in Ex2.84.
   
+(define (drop val)
+  (let ((dropped (project val))
+        (raised (raise dropped)))
+    (if (and (not (eq? (type-tag val) (type-tag dropped)))
+             (eq? val raised))
+        (drop dropped)
+        val)))
+
+;; apply-generic should simplify results; copying from 2.84
+(define (apply-generic op . args)
+  (define (solve)
+    (let ((type-tags (map type-tag args)))
+      (let ((proc (get op type-tags))
+            (coerced-list (coerce-full-list type-tags args)))
+        (cond (proc (apply proc (map contents args)))
+              (coerced-list (apply-generic op coerced-list))
+              (else (error "No method for these types"
+                           (list op type-tags)))))))
+  (let ((result (solve)))
+    (if (and (pair? result)
+             (memq (type-tag result) type-tower))
+        (drop result)
+        result)))
