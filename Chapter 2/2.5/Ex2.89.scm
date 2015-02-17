9aa8b7dc22cf73f59edeea23673c27bbf66412eb
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-02-17 08:54:21 -0800

    Ex2.89 completed

--- Chapter 2/2.5/Ex2.89.scm
+++ Chapter 2/2.5/Ex2.89.scm
@@ -4,41 +4,50 @@
   (map (lambda (term) (coeff term))
        (term-list poly)))
 
+(define (make-term order coeff) coeff)
+(define (coeff term) term)
+
 ;; Procedures
-(define (order val termlist) 
+(define (order2 val termlist) 
   (- (length termlist)
      (length (memq val (reverse termlist)))))
 
-(define (order2 val termlist)
-  (- (length (memq val termlist))
-     1))
+(define (order termlist)
+  (- (length termlist) 1))
 
-(define (adjoin-term coeff coeff-list)
-  (if (=zero? coeff)
-      coeff-list
-      (cons coeff term-list)))
+(define (zero-coeff (make-integer 0)))
+(define (adjoin-term term-order term-coeff term-list)
+  (cond ((zero? term-coeff) term-list)
+        ((= term-order (+ 1 (order term-list)))
+         (cons term-coeff term-list))
+        ((> term-order (order term-list))
+         (adjoin-term term-order term-coeff
+                      (cons zero-coeff term-list)))
+        (else (error "Cannot adjoin term with a lower order to term list"
+                     (list term-order term-coeff term-list)))))
 
 ;;Procedures rewritten
 (define (add-terms L1 L2)
   (cond ((empty-termlist? L1) L2)
         ((empty-termlist? L2) L1)
         (else
-         (let ((t1 (first-term L1)) 
-               (t2 (first-term L2)))
-           (cond ((> (order t1 L1) (order t2 L2))
+         (let ((t1 (first-term L1))
+               (o1 (order L1))
+               (t2 (first-term L2))
+               (o2 (order L2)))
+           (cond ((> o1 o2)
                   (adjoin-term
-                   t1 
-                   (add-terms (rest-terms L1) 
-                              L2)))
-                 ((< (order t1 L1) (order t2 L2))
+                   o1 t1                   
+                   (add-terms (rest-terms L1) L2)))
+                 ((< o1 o2)
                   (adjoin-term
-                   t2 
+                   o2 t2 
                    (add-terms 
                     L1 
                     (rest-terms L2))))
                  (else
                   (adjoin-term
-                   (add t1 t2)
+                   o1 (add t1 t2)
                    (add-terms 
                     (rest-terms L1)
                     (rest-terms L2)))))))))
@@ -48,17 +57,21 @@
       (the-empty-termlist)
       (add-terms 
        (mul-term-by-all-terms 
-        (first-term L1) (order (first-term L1) L1) L2)
+        (first-term L1) (order L1) L2)
        (mul-terms (rest-terms L1) L2))))
 
-(define (mul-term-by-all-terms coeff order L)
+(define (mul-term-by-all-terms term-coeff term-order L)
   (if (empty-termlist? L)
       (the-empty-termlist)
-      (let ((t2 (first-term L)))
+      (let ((t2 (first-term L))
+            (new-order (add term-order (order L)))
         (adjoin-term
-         (make-term 
-          (+ (order t1) (order t2))
-          (mul (coeff t1) (coeff t2)))
-         (mul-term-by-all-terms 
-          t1 
-          (rest-terms L))))))
+         new-order
+         (mul term-coeff t2)
+         (mul-term-by-all-terms term-coeff term-order (rest-terms L)))))))
+
+(define (negate-terms L)
+  (if (empty-termlist? L)
+      (the-empty-termlist)
+      (adjoin-term (order L) (- (first-term L))
+                   (negate-terms (rest-terms L)))))
