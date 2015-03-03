66e1538d0d8d7fd6e1337ecd2f1cbf3e624cde29
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-03-03 08:33:37 -0800

    Ex 3.01.scm

---
+++ Chapter 3/3.1/3.01.scm
@@ -0,0 +1,12 @@
+#lang planet neil/sicp
+
+(define (make-accumulator value)
+  (lambda (new-value)
+    (begin (set! value (+ value new-value))
+           value)))
+
+(define A (make-accumulator 5))
+
+(A 10)
+
+(A 10)
