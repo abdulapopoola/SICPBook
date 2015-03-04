e8d5c9b3e7165e04c024486e27bdec2d8c9152b3
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-03-04 07:50:13 -0800

    3.03.scm

---
+++ Chapter 3/3.1/3.03.scm
@@ -0,0 +1,26 @@
+#lang planet neil/sicp
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance 
+                     (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch password-value m)
+    (if (eq? password-value password)
+        (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              (else (error "Unknown request: 
+                 MAKE-ACCOUNT" m)))
+        (error "Incorrect password")))
+  dispatch)
+
+(define acc (make-account 100 'secret-password))
+
+((acc 'secret-password 'withdraw) 40)
+
+((acc 'some-other-password 'deposit) 50)
