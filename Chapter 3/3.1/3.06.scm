b1e5530e3b24046c7a950ded5c2e91dac71868a1
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-03-05 08:17:16 -0800

    3.06.scm

---
+++ Chapter 3/3.1/3.06.scm
@@ -0,0 +1,18 @@
+#lang planet neil/sicp
+
+(define rand
+  (let ((x random-init))
+    (lambda () (set! x (rand-update x)) x)))
+
+(define (rand)
+  (let ((x random-init))
+    (define (generate) 
+      (set! x (rand-update x))
+      x)
+    (define (reset new-value)
+      (set! x new-value))
+    (define (dispatch m)
+      (cond ((eq? m 'generate) generate)
+            ((eq? m 'reset) reset)
+            (else (error "Unknown request: random" m))))
+      dispatch))
