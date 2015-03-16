a204073008a61c6e256535b022371395b67f1e69
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-03-16 08:49:11 -0700

    Ex3.14.scm

---
+++ Chapter 3/3.3/Ex3.14.scm
@@ -0,0 +1,16 @@
+#lang planet neil/sicp
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+
+(define w (mystery v))
+
+;; loop reversal
