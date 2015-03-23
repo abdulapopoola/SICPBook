5c8c435b93d362eaa537d71228753493a92fe5ec
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-03-23 08:27:22 -0700

    Ex3.21.scm

---
+++ Chapter 3/3.3/Ex3.21.scm
@@ -0,0 +1,59 @@
+#lang planet neil/sicp
+
+;; Helpers
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) 
+  (set-car! queue item))
+(define (set-rear-ptr! queue item) 
+  (set-cdr! queue item))
+(define (empty-queue? queue) 
+  (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an 
+              empty queue" queue)
+      (car (front-ptr queue))))
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else (set-cdr! (rear-ptr queue) 
+                          new-pair)
+                (set-rear-ptr! queue new-pair)
+                queue))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with 
+                 an empty queue" queue))
+        (else (set-front-ptr! 
+               queue 
+               (cdr (front-ptr queue)))
+              queue)))
+
+;; Ben sees the result because the default representation
+;; returns the cons object and the queue implementation
+;; does not bother to clear the rear-pointer
+
+(define (print-queue queue)
+  (if (empty-queue? queue)
+      (display '())
+      (display (front-ptr queue))))
+
+(define q1 (make-queue))
+(print-queue q1)
+
+(insert-queue! q1 'a)
+(print-queue q1)
+
+(insert-queue! q1 'b)
+(print-queue q1)
+
+(delete-queue! q1)
+(print-queue q1)
+
+(delete-queue! q1)
+(print-queue q1)
