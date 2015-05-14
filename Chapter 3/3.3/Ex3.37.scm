901b566143e64cfdd9eb2723bae06ead25ac49a5
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-05-13 20:10:08 -0700

    Ex3.37.scm

---
+++ Chapter 3/3.3/Ex3.37.scm
@@ -0,0 +1,27 @@
+#lang planet neil/sicp
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (c- x y)
+  (let ((z (make-connector)))
+    (adder y z x)
+    z))
+
+(define (c* x y)
+  (let ((z (make-connector)))
+    (multiplier x y z)
+    z))
+
+(define (c/ x y)
+  (let ((z (make-connector)))
+    (multiplier y z x)
+    z))
+
+(define (cv const)
+  (let ((z (make-connector)))
+    (constant const z)
+    z))
+
