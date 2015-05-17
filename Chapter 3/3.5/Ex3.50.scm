461556ebb70382b26c08a6fc1f34035d50021e56
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-05-17 13:09:42 -0700

    Ex3.50.scm

---
+++ Chapter 3/3.5/Ex3.50.scm
@@ -0,0 +1,11 @@
+#lang planet neil/sicp
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (stream-cons
+       (apply proc (map stream-car argstreams))
+       (apply stream-map
+              (cons proc 
+                    (map stream-cdr 
+                         argstreams))))))
