e0da08e96b64ef84bddab943a6c815b905dda975
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-06-03 20:44:07 -0700

    Ex3.59.scm

--- Chapter 3/3.5/Ex3.59.scm
+++ Chapter 3/3.5/Ex3.59.scm
@@ -1,8 +1,22 @@
 #lang racket
 
 (define (integrate-series S)
-  (define (update-coeff divisor)
+  (define (iter divisor)
     (cons-stream 
      (/ (stream-first S) divisor)
-     (stream-rest S)))
-  (stream-cons 
+     (update-coeff (+ divisor 1))))
+  (iter 1))
+
+;; cleaner way
+(define (integrate-series2 S)
+  (map-stream / S integers))
+
+(define exp-series
+  (cons-stream 
+   1 (integrate-series exp-series)))
+
+(define cosine-series 
+  (cons-stream 1 (scale-stream (integrate-series sine-series) -1)))
+
+(define sine-series
+  (cons-stream 0 (integrate-series cosine-series)))
