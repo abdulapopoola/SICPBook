06de0dfc557d5d2a6a196f7da80d4ed3e7ebff05
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-06-03 20:55:41 -0700

    Ex3.60.scm

---
+++ Chapter 3/3.5/Ex3.60.scm
@@ -0,0 +1,7 @@
+#lang racket
+
+(define (mul-series s1 s2)
+  (cons-stream 
+   (* (stream-first s1) (stream-first s2))
+   (add-streams (scale-stream (stream-rest s2) (stream-first s1))
+                (mul-series (stream-rest s1) s2)))
