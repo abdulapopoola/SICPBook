2973b28d5751eb58ef1a470c6ed1491e45948c89
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-06-07 11:28:02 -0700

    Ex3.66.scm

---
+++ Chapter 3/3.5/Ex3.66.scm
@@ -0,0 +1,32 @@
+#lang racket
+
+(define (integers-starting-from n)
+  (stream-cons n (integers-starting-from (+ n 1))))
+
+(define integers (integers-starting-from 1))
+
+(define (interleave s1 s2)
+  (if (stream-empty? s1)
+      s2
+      (stream-cons 
+       (stream-first s1)
+       (interleave s2 (stream-rest s1)))))
+
+(define (pairs s t)
+  (stream-cons
+   (list (stream-first s) (stream-first t))
+   (interleave
+    (stream-map (lambda (x) 
+                  (list (stream-first s) x))
+                (stream-rest t))
+    (pairs (stream-rest s) (stream-rest t)))))
+
+(define (print-n stream n)
+  (newline)
+  (if (= n 0)
+      (display "Done")
+      (begin (display (stream-first stream))
+             (print-n (stream-rest stream) (- n 1)))))
+
+(define x (pairs integers integers))
+(print-n x 100)
