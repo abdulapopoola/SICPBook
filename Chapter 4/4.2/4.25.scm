03bc06a109a37333819f3519e011f13def64316d
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-08-04 20:24:38 -0700

    4.25.scm update

--- Chapter 4/4.2/4.25.scm
+++ Chapter 4/4.2/4.25.scm
@@ -22,3 +22,21 @@
 ;; This will stop since once the condition is true, there is no need to 
 ;; evaluate both halves.
 
+;; in fact the only reason why if works and is not stuck is because if is
+;; primitive procedure in the language.
+;; See example below for proof
+
+(define (if2 condition 
+                usual-value 
+                exceptional-value)
+  (if condition 
+      usual-value
+      exceptional-value))
+
+(define (factorial2 n)
+  (if2 (= n 1)
+          1
+          (* n (factorial (- n 1)))))
+
+(factorial2 5)
+;; Also never-ending loop
