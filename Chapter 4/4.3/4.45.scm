40a63eab653f474faf36ae3fdd21206d6a95f67c
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-08-17 08:42:37 -0700

    4.45.scm

---
+++ Chapter 4/4.3/4.45.scm
@@ -0,0 +1,17 @@
+;; Original sentence
+
+; 1.“The professor lectures to the student in the class with the cat.”
+; The professor lectures to the student with the cat in the class"
+
+; 2.“The professor lectures to the student in the class with the cat.”
+; The professor with the cat lectures to the student in the class
+
+; 3.“The professor lectures to the student in the class with the cat.”
+; The professor lectures in the class with the cat to the student
+
+; 4.“The professor lectures to the student in the class with the cat.”
+; The professor in the class lectures to the student with the cat
+
+; 5.“The professor lectures to the student in the class with the cat.”
+; The professor in the class with the cat lectures to the student
+
