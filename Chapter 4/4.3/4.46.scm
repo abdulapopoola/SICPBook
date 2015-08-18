fa9815edecd5665fabab6d7fe5a948adcbd3c068
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-08-18 07:41:17 -0700

    4.46.scm

---
+++ Chapter 4/4.3/4.46.scm
@@ -0,0 +1,6 @@
+; It would fail because sentences have a particular structure and pattern.
+; English for example is a left to right language and has to be parsed
+; as such otherwise, the parsing would result in gibberish.
+
+; Were the language to be Arabic (a RTL language), then the parser would
+; have to evaluate from right to left to get everything correctly.
