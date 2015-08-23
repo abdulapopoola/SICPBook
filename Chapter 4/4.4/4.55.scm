30e3651f6702cc7dec16eac8a376790edda434e0
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-08-23 11:35:41 -0700

    4.55.scm

---
+++ Chapter 4/4.4/4.55.scm
@@ -0,0 +1,10 @@
+#lang racket
+
+; all people supervised by Ben Bitdiddle;
+(supervisor ?x (Ben Bitdiddle))
+
+; the names and jobs of all people in the accounting division;
+(job ?x (accounting . ?a))
+
+; the names and addresses of all people who live in Slumerville.
+(address ?x (Slumerville . ?a))
