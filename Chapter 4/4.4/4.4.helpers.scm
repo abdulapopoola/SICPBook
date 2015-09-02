3e761064732ef9e590c7cc73eda41476be181de4
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-09-02 07:38:14 -0700

    Query implementation

---
+++ Chapter 4/4.4/4.4.helpers.scm
@@ -0,0 +1,26 @@
+(define input-prompt  ";;; Query input:")
+(define output-prompt ";;; Query results:")
+
+(define (query-driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((q (query-syntax-process (read))))
+    (cond ((assertion-to-be-added? q)
+           (add-rule-or-assertion! 
+            (add-assertion-body q))
+           (newline)
+           (display 
+            "Assertion added to data base.")
+           (query-driver-loop))
+          (else
+           (newline)
+           (display output-prompt)
+           (display-stream
+            (stream-map
+             (lambda (frame)
+               (instantiate
+                q
+                frame
+                (lambda (v f)
+                  (contract-question-mark v))))
+             (qeval q (singleton-stream '()))))
+           (query-driver-loop)))))
