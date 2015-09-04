4482648d099d8590e1fd3b66f51fe06d37acab90
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-09-03 20:38:49 -0700

    Query implementation

--- Chapter 4/4.4/4.4.helpers.scm
+++ Chapter 4/4.4/4.4.helpers.scm
@@ -24,3 +24,45 @@
                   (contract-question-mark v))))
              (qeval q (singleton-stream '()))))
            (query-driver-loop)))))
+
+(define (instantiate 
+         exp frame unbound-var-handler)
+  (define (copy exp)
+    (cond ((var? exp)
+           (let ((binding 
+                  (binding-in-frame 
+                   exp frame)))
+             (if binding
+                 (copy 
+                  (binding-value binding))
+                 (unbound-var-handler 
+                  exp frame))))
+          ((pair? exp)
+           (cons (copy (car exp)) 
+                 (copy (cdr exp))))
+          (else exp)))
+  (copy exp))
+
+(define (qeval query frame-stream)
+  (let ((qproc (get (type query) 'qeval)))
+    (if qproc
+        (qproc (contents query) frame-stream)
+        (simple-query query frame-stream))))
+
+(define (simple-query query-pattern 
+                      frame-stream)
+  (stream-flatmap
+   (lambda (frame)
+     (stream-append-delayed
+      (find-assertions query-pattern frame)
+      (delay 
+        (apply-rules query-pattern frame))))
+   frame-stream))
+
+(define (conjoin conjuncts frame-stream)
+  (if (empty-conjunction? conjuncts)
+      frame-stream
+      (conjoin (rest-conjuncts conjuncts)
+               (qeval 
+                (first-conjunct conjuncts)
+                frame-stream))))
