0671ed658a7e0f48d741e5a21e0b195a0816dd5d
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-09-04 22:20:38 -0700

    helpers

--- Chapter 4/4.4/4.4.helpers.scm
+++ Chapter 4/4.4/4.4.helpers.scm
@@ -66,3 +66,16 @@
                (qeval 
                 (first-conjunct conjuncts)
                 frame-stream))))
+
+(define (disjoin disjuncts frame-stream)
+  (if (empty-disjunction? disjuncts)
+      the-empty-stream
+      (interleave-delayed
+       (qeval (first-disjunct disjuncts) 
+              frame-stream)
+       (delay (disjoin 
+               (rest-disjuncts disjuncts)
+               frame-stream)))))
+(put 'or 'qeval disjoin)
+
+
