9771669a72d5670ce0462ace72f84acacaf7a415
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-09-28 07:34:54 -0700

    5.13.scm

---
+++ Chapter 5/5.2/5.13.scm
@@ -0,0 +1,56 @@
+#lang planet neil/sicp
+
+(define (make-machine ops controller-text)
+  (let ((machine (make-new-machine)))
+    ((machine 'install-operations) ops)
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack)))
+    (let ((the-ops
+           (list  (list 'initialize-stack
+                  (lambda () (stack 'initialize)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (allocate-register name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) 
+                 (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) 
+               allocate-register)
+              ((eq? message 'get-register) 
+               lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) 
+                 (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) 
+               the-ops)
+              (else (error "Unknown request: MACHINE" message))))
+      dispatch)))
