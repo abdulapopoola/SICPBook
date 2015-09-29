247eafbcd1411ef5e3c0c7576dcd408ba727010c
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-09-29 07:25:43 -0700

    5.16.scm

---
+++ Chapter 5/5.2/5.16.scm
@@ -0,0 +1,59 @@
+#lang planet neil/sicp
+
+;; Changes highlighted with ;; **
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+        (trace-instructions false)) ;; **
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 (list 'print-stack-statistics
+                       (lambda () 
+                         (stack 'print-statistics)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                (if trace-instructions ;; **
+                    (display "TRACE: " (instruction-text (car insts))))
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) 
+                 (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) 
+                 (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              ((eq? message 'trace-on) ;; **
+               (set! trace-instructions true))
+              ((eq? message 'trace-off) ;; **
+               (set! trace-instructions false))
+              (else (error "Unknown request: MACHINE" message))))
+      dispatch)))
