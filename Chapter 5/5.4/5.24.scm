053c39579ba2455f7883f9ab6c52da42c9cb3e4a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-10-11 09:25:29 -0700

    5.24.scm

--- Chapter 5/5.4/5.24.scm
+++ Chapter 5/5.4/5.24.scm
@@ -7,11 +7,34 @@
 
 ev-cond
    (assign unev (op cond-clauses) (reg exp))
+   (save continue)
 ev-cond-loop
-   (assign exp (op first-exp) (reg exp))
-   (test ) ;;if exp is true)
+   (assign exp (op first-exp) (reg unev))
+   (test (op cond-else-clause?) (reg exp)) ;;if is else clause, go to action
    (branch (label ev-cond-action))
    (save unev)
    (save env)
+   ;;Now exp only contains predicate expression of cond clause
+   (assign exp (op cond-predicate) (reg exp))
+   (assign continue (label ev-cond-clause))
+   (goto (label eval-dispatch))
+   
+;; Execution will only get here once predicate has been evaluated
+;; i.e. from line 19 above
+ev-cond-clause
+   (restore unev)
+   (restore env)
+   (test (op true?) (reg val))
+   (branch (label ev-cond-action))
+   ;; Cond predicate was falsy, update unev to point to remaining clauses
+   (assign unev (op cond-clauses) (reg unev))
+   (goto (label ev-cond-loop))
+   
 ev-cond-action
+   ; Only take the first exp as it is the one that eval to true or is else 
+   ; clause. 
+   ; This approach helps to avoid saving the exp and using continue
+   (assign unev (op first-exp) (reg unev))
+   (assign unev (op cond-actions) (reg unev))
+   (goto (label ev-sequence))
    
