285ce1cade41eb9535cdcbda2b2f6a0a59bd1a12
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-10-12 07:43:21 -0700

    5.25.scm skipped

---
+++ Chapter 5/5.4/5.25.scm
@@ -0,0 +1,6 @@
+#lang planet neil/sicp
+
+;; Skipping this exercise
+;; However Skanev has a solution here (if you are interested)
+;; https://github.com/skanev/playground/blob/master/scheme/sicp/05/25.scm
+
