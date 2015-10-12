ac919d8877f5db7dbdce5c59f4ac4c3df1800e17
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-10-12 08:07:22 -0700

    5.26.scm in progress

--- Chapter 5/5.4/5.4.helpers.scm
+++ Chapter 5/5.4/5.4.helpers.scm
@@ -1,4 +1,9 @@
-eval-dispatch
+(define eceval
+  (make-machine
+   '(exp env val proc argl continue unev)
+   eceval-operations
+   '(
+     eval-dispatch
   (test (op self-evaluating?) (reg exp))
   (branch (label ev-self-eval))
   (test (op variable?) (reg exp))
@@ -232,3 +237,44 @@ ev-definition-1
            (reg env))
   (assign val (const ok))
   (goto (reg continue))
+
+read-eval-print-loop
+  (perform (op initialize-stack))
+  (perform (op prompt-for-input)
+           (const ";;; EC-Eval input:"))
+  (assign exp (op read))
+  (assign env (op get-global-environment))
+  (assign continue (label print-result))
+  (goto (label eval-dispatch))
+print-result
+  (perform (op announce-output)
+           (const ";;; EC-Eval value:"))
+  (perform (op user-print) (reg val))
+  (goto (label read-eval-print-loop))
+  
+unknown-expression-type
+  (assign 
+   val
+   (const unknown-expression-type-error))
+  (goto (label signal-error))
+unknown-procedure-type
+  ; clean up stack (from apply-dispatch):
+  (restore continue)    
+  (assign 
+   val
+   (const unknown-procedure-type-error))
+  (goto (label signal-error))
+signal-error
+  (perform (op user-print) (reg val))
+  (goto (label read-eval-print-loop)))))
+
+(define eceval-operations
+  (list (list 'self-evaluating? 
+              self-evaluating)
+        ⟨complete list of operations 
+         for eceval machine⟩))
+
+(define the-global-environment
+  (setup-environment))
+
+(start eceval)
