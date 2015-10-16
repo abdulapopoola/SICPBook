920ac06997d4e99213fb4175ceb199375b4ae893
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-10-16 07:38:23 -0700

    5.26.scm solved (finally)

--- Chapter 5/5.4/5.26.scm
+++ Chapter 5/5.4/5.26.scm
@@ -11,3 +11,17 @@
 ;; The answers were gotten by running loader.scm and deducing the answers
 
 ;; 1. Max depth 10
+
+;; 2. 
+
++-------+--------+
+|   n   | pushes |
++-------+--------+
+|     1 |     64 |
+|    10 |    379 |
+|   100 |   3529 |
+|  1000 |  35029 |
+| 10000 | 350029 |
++-------+--------+
+
+;; Formula for pushes is ~ 35n + 29
