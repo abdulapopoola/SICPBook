de7d02544f4c5f41e462478add45aead60beaf2e
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2015-10-25 08:09:40 -0700

    More compiler methods

--- Chapter 5/5.5/compiler.scm
+++ Chapter 5/5.5/compiler.scm
@@ -25,3 +25,114 @@
 (define (make-instruction-sequence 
          needs modifies statements)
   (list needs modifies statements))
+
+(define (empty-instruction-sequence)
+  (make-instruction-sequence '() '() '()))
+
+(define (compile-linkage linkage)
+  (cond ((eq? linkage 'return)
+         (make-instruction-sequence 
+          '(continue) '() '((goto (reg continue)))))
+        ((eq? linkage 'next)
+         (empty-instruction-sequence))
+        (else
+         (make-instruction-sequence '() '()
+          `((goto (label ,linkage)))))))
+
+(define (end-with-linkage 
+         linkage instruction-sequence)
+  (preserving '(continue)
+   instruction-sequence
+   (compile-linkage linkage)))
+
+(define (compile-self-evaluating exp target linkage)
+  (end-with-linkage
+   linkage (make-instruction-sequence 
+            '()
+            (list target)
+            `((assign ,target (const ,exp))))))
+
+(define (compile-quoted exp target linkage)
+  (end-with-linkage
+   linkage (make-instruction-sequence
+            '()
+            (list target)
+            `((assign ,target (const ,(text-of-quotation exp)))))))
+
+(define (compile-variable
+         exp target linkage)
+  (end-with-linkage 
+   linkage (make-instruction-sequence
+            '(env)
+            (list target)
+            `((assign ,target
+                      (op lookup-variable-value)
+                      (const ,exp)
+                      (reg env))))))
+
+(define (compile-assignment exp target linkage)
+  (let ((var (assignment-variable exp))
+        (get-value-code
+         (compile (assignment-value exp) 'val 'next)))
+    (end-with-linkage 
+     linkage
+     (preserving 
+      '(env)
+      get-value-code
+      (make-instruction-sequence
+       '(env val)
+       (list target)
+       `((perform (op set-variable-value!)
+                  (const ,var)
+                  (reg val)
+                  (reg env))
+         (assign ,target (const ok))))))))
+
+(define (compile-definition exp target linkage)
+  (let ((var (definition-variable exp))
+        (get-value-code
+         (compile (definition-value exp) 'val 'next)))
+    (end-with-linkage
+     linkage
+     (preserving 
+      '(env)
+      get-value-code
+      (make-instruction-sequence
+       '(env val)
+       (list target)
+       `((perform (op define-variable!)
+                  (const ,var)
+                  (reg val)
+                  (reg env))
+         (assign ,target (const ok))))))))
+
+(define (compile-if exp target linkage)
+  (let ((t-branch (make-label 'true-branch))
+        (f-branch (make-label 'false-branch))
+        (after-if (make-label 'after-if)))
+    (let ((consequent-linkage
+           (if (eq? linkage 'next) 
+               after-if
+               linkage)))
+      (let ((p-code 
+             (compile (if-predicate exp) 'val 'next))
+            (c-code
+             (compile (if-consequent exp) target consequent-linkage))
+            (a-code
+             (compile (if-alternative exp) target linkage)))
+        (preserving 
+         '(env continue)
+         p-code
+         (append-instruction-sequences
+          (make-instruction-sequence 
+           '(val) 
+           '()
+           `((test (op false?) (reg val))
+             (branch (label ,f-branch))))
+          (parallel-instruction-sequences
+           (append-instruction-sequences 
+            t-branch c-code)
+           (append-instruction-sequences
+            f-branch a-code))
+          after-if))))))
+
